da284389e4a76facb406515e0b30353f584415c2
Aria Moradi <aria.moradi007@gmail.com>
2021-08-07 21:38:38 +0430

    optimze imports

--- lib/src/main/kotlin/de/neonew/exposed/migrations/helpers/RenameFieldMigration.kt
+++ lib/src/main/kotlin/de/neonew/exposed/migrations/helpers/RenameFieldMigration.kt
@@ -7,10 +7,6 @@ package de.neonew.exposed.migrations.helpers
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import de.neonew.exposed.migrations.Migration
-import org.jetbrains.exposed.sql.transactions.TransactionManager
-import org.jetbrains.exposed.sql.vendors.currentDialect
-
 abstract class RenameFieldMigration(
     tableName: String,
     originalName: String,
