b357710f01bb81fecc8b3fa56f62ad71ce992ca2
schroda <50052685+schroda@users.noreply.github.com>
2024-11-18 01:50:07 +0100

    Update to kotlin-logging v7.0.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     // Logging
-    compileOnly("io.github.oshai:kotlin-logging:6.0.9")
+    compileOnly("io.github.oshai:kotlin-logging-jvm:7.0.0")
 
     // Exposed ORM
     val exposedVersion = "0.56.0"
